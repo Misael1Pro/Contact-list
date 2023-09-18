ac9d05181b63db72f9abf5765bc1fa19115852b2
Misael <betancourthmisael2001@gmail.com>
2023-09-17 20:44:33 -0600

    Lista de contactos con objeto

--- ListaContactos.js
+++ ListaContactos.js
@@ -1,15 +1,40 @@
- let ListaContactos= [ "MisaelBetancourth","JoseLopez","DanielaBetancourth"];
+ let ListaContactos =[
+  {
+    id:1,
+    nombre:"Misael", 
+    Apellido: "Betancourth", 
+    telefono: "97206172",
+    ubicacion:{Ciudad: "Tegucigalpa", Direccion: "Res. Aleman"}
+  },
+  {
+    id:2,
+    nombre:"Jose", 
+    Apellido: "Bonilla", 
+    telefono: "98276832",
+    ubicacion:{Ciudad: "Matagalpa", Direccion: "Res. plaza"}
+  },
+  
+  ];
+  let contNuevo = {
+    id:3,
+    nombre:"Asael", 
+    Apellido: "Berrios", 
+    telefono: "9334523",
+    ubicacion:{Ciudad: "Jutiapa", Direccion: "Barrio Mora"}
+  }
+   function AgregarContactos (NuevoContacto){
+     ListaContactos.push(NuevoContacto);
+     return ListaContactos;
+   }
+   function BorrarContacto (EliminarContacto){
+   let nuevalista= ListaContactos.filter((contacto)=> contacto.id !==EliminarContacto)
+   ListaContactos=nuevalista
+   }
+  function imprimircontacto(ListaContactos){
+      consola.log(ListaContactos)
+  }
 
-function AgregarContacto (Nuevocontacto){
-ListaContactos.push(Nuevocontacto)
-}
-function BorrarContacto (EliminarContacto){
-  let nuevalista= ListaContactos.filter((contacto)=> contacto !==EliminarContacto)
-  ListaContactos=nuevalista
-}
-function imprimircontacto(ListaContactos){
-    console.log(ListaContactos)
-}
-AgregarContacto("Jose Garcia")  
-BorrarContacto("MisaelBetancourth")
-imprimircontacto(ListaContactos)
+//  AgregarContactos(contNuevo) 
+
+BorrarContacto(2)
+console.log(ListaContactos)
